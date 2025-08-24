5de6a549a6c9ed30c9c4ee30bdd8d0395bee73c9
Auriel James Fernandez <aurieljames11@gmail.com>
2025-08-24 09:03:20 +0800

    Update padding

--- src/components/HealthRiskDetector.tsx
+++ src/components/HealthRiskDetector.tsx
@@ -185,7 +185,7 @@ export default function HealthRiskDetector() {
         </div>
       </div>
 
-      <div className="max-w-7xl mx-auto p2 sm:p-2 md:p-4 sm:px-6 lg:px-8 py-8">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
 
           {/* Form Section */}
